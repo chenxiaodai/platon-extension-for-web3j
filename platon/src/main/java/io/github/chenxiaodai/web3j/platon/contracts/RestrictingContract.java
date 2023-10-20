cb0e3e2006d20c1e1e16995ba5425260f8c04044
chenxiaodai <chenxiaodai@126.com>
2023-10-20 14:39:09 +0800

    增加锁仓合约

---
+++ platon/src/main/java/io/github/chenxiaodai/web3j/platon/contracts/RestrictingContract.java
@@ -0,0 +1,99 @@
+package io.github.chenxiaodai.web3j.platon.contracts;
+
+import io.github.chenxiaodai.web3j.platon.contracts.common.Function;
+import io.github.chenxiaodai.web3j.platon.contracts.dto.CallResponse;
+import io.github.chenxiaodai.web3j.platon.contracts.dto.TransactionResponse;
+import io.github.chenxiaodai.web3j.platon.contracts.dto.req.RestrictingPlan;
+import io.github.chenxiaodai.web3j.platon.contracts.dto.resp.RestrictingItem;
+import io.github.chenxiaodai.web3j.platon.contracts.enums.InnerContractEnum;
+import io.github.chenxiaodai.web3j.platon.contracts.type.HexStringType;
+import io.github.chenxiaodai.web3j.platon.contracts.type.ListType;
+import io.github.chenxiaodai.web3j.platon.contracts.type.Type;
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.RemoteCall;
+import org.web3j.tx.TransactionManager;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RestrictingContract extends BaseContract {
+
+    public final static int FUNC_CREATE_RESTRICTING_PLAN = 4000;
+    public final static int FUNC_GET_RESTRICTING_INFO = 4100;
+
+	/**
+	 * 加载合约, 默认ReadonlyTransactionManager事务管理
+	 *
+	 * @param web3j 客户端
+	 * @return staking合约
+	 */
+    public static RestrictingContract load(Web3j web3j) {
+        return new RestrictingContract(InnerContractEnum.RESTRICTING_CONTRACT.getAddress(), web3j);
+    }
+
+    /**
+     * 加载合约
+     *
+     * @param web3j 客户端
+     * @param transactionManager 交易管理
+     * @return staking合约
+     */
+    public static RestrictingContract load(Web3j web3j, TransactionManager transactionManager) {
+    	return new RestrictingContract(InnerContractEnum.RESTRICTING_CONTRACT.getAddress(), web3j, transactionManager);
+    }
+
+    /**
+     * 加载合约, 默认RawTransactionManager事务管理
+     *
+     * @param web3j 客户端
+     * @param credentials 钱包
+     * @param chainId 链id
+     * @return staking合约
+     */
+    public static RestrictingContract load(Web3j web3j, Credentials credentials, long chainId) {
+    	return new RestrictingContract(InnerContractEnum.RESTRICTING_CONTRACT.getAddress(), web3j, credentials, chainId);
+    }
+
+    private RestrictingContract(String contractAddress, Web3j web3j) {
+        super(contractAddress, web3j);
+    }
+
+    private RestrictingContract(String contractAddress, Web3j web3j, Credentials credentials, long chainId) {
+        super(contractAddress, web3j, credentials, chainId);
+    }
+
+    private RestrictingContract(String contractAddress, Web3j web3j, TransactionManager transactionManager) {
+        super(contractAddress, web3j, transactionManager);
+    }
+
+    /**
+     * 创建锁仓计划
+     *
+     * @param account 锁仓释放到账账户
+     * @param planList 锁仓计划
+     * @return 返回交易回执信息
+     */
+    public RemoteCall<TransactionResponse> createRestrictingPlan(String account, List<RestrictingPlan> planList) {
+        List<Type> param = Arrays.asList(
+                new HexStringType(account),
+                new ListType<>(planList)
+        );
+        Function function = new Function(FUNC_CREATE_RESTRICTING_PLAN, param);
+        return executeRemoteCallTransaction(function);
+    }
+
+    /**
+     * 获取锁仓信息
+     *
+     * @param account 锁仓释放到账账户
+     * @return 提案列表
+     */
+    public RemoteCall<CallResponse<RestrictingItem>> getRestrictingInfo(String account) {
+        List<Type> param = Arrays.asList(
+                new HexStringType(account)
+        );
+        Function function = new Function(FUNC_GET_RESTRICTING_INFO, param);
+        return executeRemoteCallSingleValueReturn(function, RestrictingItem.class);
+    }
+}
---
+++ platon/src/main/java/io/github/chenxiaodai/web3j/platon/contracts/type/ListType.java
@@ -0,0 +1,30 @@
+package io.github.chenxiaodai.web3j.platon.contracts.type;
+
+import org.web3j.rlp.RlpList;
+import org.web3j.rlp.RlpType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListType<T extends Type> implements Type<List<T>> {
+
+    private List<T> value;
+
+    public ListType(List<T> value) {
+        this.value = value;
+    }
+
+    @Override
+    public RlpType getRlpType() {
+        List<RlpType> rlpListList = new ArrayList<>();
+        for (Type t : value) {
+            rlpListList.add(t.getRlpType());
+        }
+        return new RlpList(rlpListList);
+    }
+
+    @Override
+    public List<T> getValue() {
+        return value;
+    }
+}
